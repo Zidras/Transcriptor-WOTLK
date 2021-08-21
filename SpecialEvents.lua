d661f2563c6e5432efbd0baca13f1805ae341c01
Zidras <10605951+Zidras@users.noreply.github.com>
2021-08-22 00:47:43 +0100

    Fix special events missing index

--- SpecialEvents.lua
+++ SpecialEvents.lua
@@ -4,4 +4,21 @@ do
 	_, tbl = ...
 end
 
-tbl.specialEvents = {}
+tbl.specialEvents = {
+	["UNIT_SPELLCAST_SUCCEEDED"] = {
+	},
+	["UNIT_SPELLCAST_INTERRUPTED"] = {
+	},
+	["SPELL_AURA_APPLIED"] = {
+	},
+	["SPELL_AURA_APPLIED_DOSE"] = {
+	},
+	["SPELL_AURA_REMOVED"] = {
+	},
+	["SPELL_CAST_START"] = {
+	},
+	["SPELL_CAST_SUCCESS"] = {
+	},
+	["UNIT_DIED"] = {
+	}
+}
